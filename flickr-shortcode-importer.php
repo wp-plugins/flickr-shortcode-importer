10ee0688fbb2b57882f45463e4f28e4c199920a4
comprock <comprock@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-11 04:43:20 +0000

    Update method names for FSI; Remove unused methods; Begin post pulling with [flickr]; Set 5-minute time limit per post; Update verbiage for FSI usage
    git-svn-id: https://plugins.svn.wordpress.org/flickr-shortcode-importer/trunk@421980 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- flickr-shortcode-importer.php
+++ flickr-shortcode-importer.php
@@ -42,9 +42,8 @@ class Flickr_Shortcode_Importer {
 
 		add_action( 'admin_menu', array( &$this, 'add_admin_menu' ) );
 		add_action( 'admin_enqueue_scripts', array( &$this, 'admin_enqueues' ) );
-		add_action( 'wp_ajax_regeneratethumbnail', array( &$this, 'ajax_process_image' ) );
+		add_action( 'wp_ajax_importflickrshortcode', array( &$this, 'ajax_process_shortcode' ) );
 		add_filter( 'plugin_action_links', array( &$this, 'add_plugin_action_links' ), 10, 2 );
-		add_action( 'admin_action_bulk_regenerate_thumbnails', array( &$this, 'bulk_action_handler' ) );
 	}
 
 
@@ -62,9 +61,10 @@ class Flickr_Shortcode_Importer {
 		return $links;
 	}
 
+
 	// Register the management page
 	function add_admin_menu() {
-		$this->menu_id = add_management_page( __( 'Flickr Shortcode Importer', 'flickr-shortcode-importer' ), __( '[flickr] Importer', 'flickr-shortcode-importer' ), 'manage_options', 'flickr-shortcode-importer', array(&$this, 'regenerate_interface') );
+		$this->menu_id = add_management_page( __( 'Flickr Shortcode Importer', 'flickr-shortcode-importer' ), __( '[flickr] Importer', 'flickr-shortcode-importer' ), 'manage_options', 'flickr-shortcode-importer', array(&$this, 'user_interface') );
 	}
 
 
@@ -79,40 +79,25 @@ class Flickr_Shortcode_Importer {
 		else
 			wp_enqueue_script( 'jquery-ui-progressbar', plugins_url( 'jquery-ui/jquery.ui.progressbar.min.1.7.2.js', __FILE__ ), array( 'jquery-ui-core' ), '1.7.2' );
 
-		wp_enqueue_style( 'jquery-ui-regenthumbs', plugins_url( 'jquery-ui/redmond/jquery-ui-1.7.2.custom.css', __FILE__ ), array(), '1.7.2' );
-	}
-
-
-	// Handles the bulk actions POST
-	function bulk_action_handler() {
-		check_admin_referer( 'bulk-media' );
-
-		if ( empty( $_REQUEST['media'] ) || ! is_array( $_REQUEST['media'] ) )
-			return;
-
-		$ids = implode( ',', array_map( 'intval', $_REQUEST['media'] ) );
-
-		// Can't use wp_nonce_url() as it escapes HTML entities
-		wp_redirect( add_query_arg( '_wpnonce', wp_create_nonce( 'flickr-shortcode-importer' ), admin_url( 'tools.php?page=flickr-shortcode-importer&goback=1&ids=' . $ids ) ) );
-		exit();
+		wp_enqueue_style( 'jquery-ui-fsiposts', plugins_url( 'jquery-ui/redmond/jquery-ui-1.7.2.custom.css', __FILE__ ), array(), '1.7.2' );
 	}
 
 
 	// The user interface plus thumbnail regenerator
-	function regenerate_interface() {
+	function user_interface() {
 		global $wpdb;
 
 		?>
 
 <div id="message" class="updated fade" style="display:none"></div>
 
-<div class="wrap regenthumbs">
+<div class="wrap fsiposts">
 	<h2><?php _e('Flickr Shortcode Importer', 'flickr-shortcode-importer'); ?></h2>
 
 <?php
 
 		// If the button was clicked
-		if ( ! empty( $_POST['flickr-shortcode-importer'] ) || ! empty( $_REQUEST['ids'] ) ) {
+		if ( ! empty( $_POST['flickr-shortcode-importer'] ) || ! empty( $_REQUEST['posts'] ) ) {
 			// Capability check
 			if ( !current_user_can( 'manage_options' ) )
 				wp_die( __( 'Cheatin&#8217; uh?' , 'flickr-shortcode-importer') );
@@ -121,52 +106,53 @@ class Flickr_Shortcode_Importer {
 			check_admin_referer( 'flickr-shortcode-importer' );
 
 			// Create the list of image IDs
-			if ( ! empty( $_REQUEST['ids'] ) ) {
-				$images = array_map( 'intval', explode( ',', trim( $_REQUEST['ids'], ',' ) ) );
-				$ids = implode( ',', $images );
+			if ( ! empty( $_REQUEST['posts'] ) ) {
+				$posts			= array_map( 'intval', explode( ',', trim( $_REQUEST['posts'], ',' ) ) );
+				$count			= count( $posts );
+				$posts			= implode( ',', $posts );
 			} else {
 				// Directly querying the database is normally frowned upon, but all
 				// of the API functions will return the full post objects which will
 				// suck up lots of memory. This is best, just not as future proof.
-				if ( ! $images = $wpdb->get_results( "SELECT ID FROM $wpdb->posts WHERE post_type = 'attachment' AND post_mime_type LIKE 'image/%' ORDER BY ID DESC" ) ) {
-					echo '	<p>' . sprintf( __( "Unable to find any images. Are you sure <a href='%s'>some exist</a>?", 'flickr-shortcode-importer' ), admin_url( 'upload.php?post_mime_type=image' ) ) . "</p></div>";
+				if ( ! $results = $wpdb->get_results( "SELECT ID FROM $wpdb->posts WHERE post_type = 'post' AND post_content LIKE '%[flickr%' ORDER BY ID DESC LIMIT 1" ) ) {
+					echo '	<p>' . _e( 'No [flickr] codes found in posts', 'flickr-shortcode-importer' ) . "</p></div>";
 					return;
 				}
 
+				$count			= count( $results );
+
 				// Generate the list of IDs
-				$ids = array();
-				foreach ( $images as $image )
-					$ids[] = $image->ID;
-				$ids = implode( ',', $ids );
+				$posts			= array();
+				foreach ( $results as $post ) {
+					$posts[]	= $post->ID;
+				}
+				$posts			= implode( ',', $posts );
 			}
 
-			echo '	<p>' . __( "Please be patient while the thumbnails are regenerated. This can take a while if your server is slow (inexpensive hosting) or if you have many images. Do not navigate away from this page until this script is done or the thumbnails will not be resized. You will be notified via this page when the regenerating is completed.", 'flickr-shortcode-importer' ) . '</p>';
-
-			$count = count( $images );
+			echo '	<p>' . __( "Please be patient while the [flickr] shortcodes are imported. This can take a while if your server is slow or if you have many [flickr] shortcodes in your post content. Do not navigate away from this page until this script is done or the import will not be completed. You will be notified via this page when the import is completed.", 'flickr-shortcode-importer' ) . '</p>';
 
 			$text_goback = ( ! empty( $_GET['goback'] ) ) ? sprintf( __( 'To go back to the previous page, <a href="%s">click here</a>.', 'flickr-shortcode-importer' ), 'javascript:history.go(-1)' ) : '';
-			$text_failures = sprintf( __( 'All done! %1$s image(s) were successfully resized in %2$s seconds and there were %3$s failure(s). To try regenerating the failed images again, <a href="%4$s">click here</a>. %5$s', 'flickr-shortcode-importer' ), "' + rt_successes + '", "' + rt_totaltime + '", "' + rt_errors + '", esc_url( wp_nonce_url( admin_url( 'tools.php?page=flickr-shortcode-importer&goback=1' ), 'flickr-shortcode-importer' ) . '&ids=' ) . "' + rt_failedlist + '", $text_goback );
-			$text_nofailures = sprintf( __( 'All done! %1$s image(s) were successfully resized in %2$s seconds and there were 0 failures. %3$s', 'flickr-shortcode-importer' ), "' + rt_successes + '", "' + rt_totaltime + '", $text_goback );
+			$text_failures = sprintf( __( 'All done! %1$s [flickr](s) were successfully imported in %2$s seconds and there were %3$s failure(s). To try importing the failed [flickr]s again, <a href="%4$s">click here</a>. %5$s', 'flickr-shortcode-importer' ), "' + rt_successes + '", "' + rt_totaltime + '", "' + rt_errors + '", esc_url( wp_nonce_url( admin_url( 'tools.php?page=flickr-shortcode-importer&goback=1' ), 'flickr-shortcode-importer' ) . '&posts=' ) . "' + rt_failedlist + '", $text_goback );
+			$text_nofailures = sprintf( __( 'All done! %1$s [flickr](s) were successfully imported in %2$s seconds and there were 0 failures. %3$s', 'flickr-shortcode-importer' ), "' + rt_successes + '", "' + rt_totaltime + '", $text_goback );
 ?>
-
 
 	<noscript><p><em><?php _e( 'You must enable Javascript in order to proceed!', 'flickr-shortcode-importer' ) ?></em></p></noscript>
 
-	<div id="regenthumbs-bar" style="position:relative;height:25px;">
-		<div id="regenthumbs-bar-percent" style="position:absolute;left:50%;top:50%;width:300px;margin-left:-150px;height:25px;margin-top:-9px;font-weight:bold;text-align:center;"></div>
+	<div id="fsiposts-bar" style="position:relative;height:25px;">
+		<div id="fsiposts-bar-percent" style="position:absolute;left:50%;top:50%;width:300px;margin-left:-150px;height:25px;margin-top:-9px;font-weight:bold;text-align:center;"></div>
 	</div>
 
-	<p><input type="button" class="button hide-if-no-js" name="regenthumbs-stop" id="regenthumbs-stop" value="<?php _e( 'Abort Resizing Images', 'flickr-shortcode-importer' ) ?>" /></p>
+	<p><input type="button" class="button hide-if-no-js" name="fsiposts-stop" id="fsiposts-stop" value="<?php _e( 'Abort Importing [flickr]s', 'flickr-shortcode-importer' ) ?>" /></p>
 
 	<h3 class="title"><?php _e( 'Debugging Information', 'flickr-shortcode-importer' ) ?></h3>
 
 	<p>
-		<?php printf( __( 'Total Images: %s', 'flickr-shortcode-importer' ), $count ); ?><br />
-		<?php printf( __( 'Images Resized: %s', 'flickr-shortcode-importer' ), '<span id="regenthumbs-debug-successcount">0</span>' ); ?><br />
-		<?php printf( __( 'Resize Failures: %s', 'flickr-shortcode-importer' ), '<span id="regenthumbs-debug-failurecount">0</span>' ); ?>
+		<?php printf( __( 'Total [flickr]: %s', 'flickr-shortcode-importer' ), $count ); ?><br />
+		<?php printf( __( '[flickr]s Imported: %s', 'flickr-shortcode-importer' ), '<span id="fsiposts-debug-successcount">0</span>' ); ?><br />
+		<?php printf( __( 'Import Failures: %s', 'flickr-shortcode-importer' ), '<span id="fsiposts-debug-failurecount">0</span>' ); ?>
 	</p>
 
-	<ol id="regenthumbs-debuglist">
+	<ol id="fsiposts-debuglist">
 		<li style="display:none"></li>
 	</ol>
 
@@ -174,8 +160,8 @@ class Flickr_Shortcode_Importer {
 	// <![CDATA[
 		jQuery(document).ready(function($){
 			var i;
-			var rt_images = [<?php echo $ids; ?>];
-			var rt_total = rt_images.length;
+			var rt_posts = [<?php echo $posts; ?>];
+			var rt_total = rt_posts.length;
 			var rt_count = 1;
 			var rt_percent = 0;
 			var rt_successes = 0;
@@ -188,43 +174,43 @@ class Flickr_Shortcode_Importer {
 			var rt_continue = true;
 
 			// Create the progress bar
-			$("#regenthumbs-bar").progressbar();
-			$("#regenthumbs-bar-percent").html( "0%" );
+			$("#fsiposts-bar").progressbar();
+			$("#fsiposts-bar-percent").html( "0%" );
 
 			// Stop button
-			$("#regenthumbs-stop").click(function() {
+			$("#fsiposts-stop").click(function() {
 				rt_continue = false;
-				$('#regenthumbs-stop').val("<?php echo $this->esc_quotes( __( 'Stopping...', 'flickr-shortcode-importer' ) ); ?>");
+				$('#fsiposts-stop').val("<?php echo $this->esc_quotes( __( 'Stopping...', 'flickr-shortcode-importer' ) ); ?>");
 			});
 
 			// Clear out the empty list element that's there for HTML validation purposes
-			$("#regenthumbs-debuglist li").remove();
+			$("#fsiposts-debuglist li").remove();
 
 			// Called after each resize. Updates debug information and the progress bar.
-			function FSIThumbsUpdateStatus( id, success, response ) {
-				$("#regenthumbs-bar").progressbar( "value", ( rt_count / rt_total ) * 100 );
-				$("#regenthumbs-bar-percent").html( Math.round( ( rt_count / rt_total ) * 1000 ) / 10 + "%" );
+			function FSIPostsUpdateStatus( id, success, response ) {
+				$("#fsiposts-bar").progressbar( "value", ( rt_count / rt_total ) * 100 );
+				$("#fsiposts-bar-percent").html( Math.round( ( rt_count / rt_total ) * 1000 ) / 10 + "%" );
 				rt_count = rt_count + 1;
 
 				if ( success ) {
 					rt_successes = rt_successes + 1;
-					$("#regenthumbs-debug-successcount").html(rt_successes);
-					$("#regenthumbs-debuglist").append("<li>" + response.success + "</li>");
+					$("#fsiposts-debug-successcount").html(rt_successes);
+					$("#fsiposts-debuglist").append("<li>" + response.success + "</li>");
 				}
 				else {
 					rt_errors = rt_errors + 1;
 					rt_failedlist = rt_failedlist + ',' + id;
-					$("#regenthumbs-debug-failurecount").html(rt_errors);
-					$("#regenthumbs-debuglist").append("<li>" + response.error + "</li>");
+					$("#fsiposts-debug-failurecount").html(rt_errors);
+					$("#fsiposts-debuglist").append("<li>" + response.error + "</li>");
 				}
 			}
 
-			// Called when all images have been processed. Shows the results and cleans up.
-			function FSIThumbsFinishUp() {
+			// Called when all posts have been processed. Shows the results and cleans up.
+			function FSIPostsFinishUp() {
 				rt_timeend = new Date().getTime();
 				rt_totaltime = Math.round( ( rt_timeend - rt_timestart ) / 1000 );
 
-				$('#regenthumbs-stop').hide();
+				$('#fsiposts-stop').hide();
 
 				if ( rt_errors > 0 ) {
 					rt_resulttext = '<?php echo $text_failures; ?>';
@@ -237,40 +223,40 @@ class Flickr_Shortcode_Importer {
 			}
 
 			// Regenerate a specified image via AJAX
-			function FSIThumbs( id ) {
+			function FSIPosts( id ) {
 				$.ajax({
 					type: 'POST',
 					url: ajaxurl,
-					data: { action: "regeneratethumbnail", id: id },
+					data: { action: "importflickrshortcode", id: id },
 					success: function( response ) {
 						if ( response.success ) {
-							FSIThumbsUpdateStatus( id, true, response );
+							FSIPostsUpdateStatus( id, true, response );
 						}
 						else {
-							FSIThumbsUpdateStatus( id, false, response );
+							FSIPostsUpdateStatus( id, false, response );
 						}
 
-						if ( rt_images.length && rt_continue ) {
-							FSIThumbs( rt_images.shift() );
+						if ( rt_posts.length && rt_continue ) {
+							FSIPosts( rt_posts.shift() );
 						}
 						else {
-							FSIThumbsFinishUp();
+							FSIPostsFinishUp();
 						}
 					},
 					error: function( response ) {
-						FSIThumbsUpdateStatus( id, false, response );
+						FSIPostsUpdateStatus( id, false, response );
 
-						if ( rt_images.length && rt_continue ) {
-							FSIThumbs( rt_images.shift() );
+						if ( rt_posts.length && rt_continue ) {
+							FSIPosts( rt_posts.shift() );
 						} 
 						else {
-							FSIThumbsFinishUp();
+							FSIPostsFinishUp();
 						}
 					}
 				});
 			}
 
-			FSIThumbs( rt_images.shift() );
+			FSIPosts( rt_posts.shift() );
 		});
 	// ]]>
 	</script>
@@ -304,26 +290,28 @@ class Flickr_Shortcode_Importer {
 
 
 	// Process a single image ID (this is an AJAX handler)
-	function ajax_process_image() {
+	function ajax_process_shortcode() {
 		@error_reporting( 0 ); // Don't break the JSON result
 
 		header( 'Content-type: application/json' );
 
 		$id = (int) $_REQUEST['id'];
 		$image = get_post( $id );
+		die( json_encode( array( 'success' => sprintf( __( '&quot;%1$s&quot; (ID %2$s) was successfully imported in %3$s seconds.', 'flickr-shortcode-importer' ), esc_html( get_the_title( $image->ID ) ), $image->ID, timer_stop() ) ) ) );
+
 
 		if ( ! $image || 'attachment' != $image->post_type || 'image/' != substr( $image->post_mime_type, 0, 6 ) )
 			die( json_encode( array( 'error' => sprintf( __( 'Failed resize: %s is an invalid image ID.', 'flickr-shortcode-importer' ), esc_html( $_REQUEST['id'] ) ) ) ) );
 
 		if ( !current_user_can( 'manage_options' ) )
-			$this->die_json_error_msg( $image->ID, __( "Your user account doesn't have permission to resize images", 'flickr-shortcode-importer' ) );
+			$this->die_json_error_msg( $image->ID, __( "Your user account doesn't have permission to import images", 'flickr-shortcode-importer' ) );
 
 		$fullsizepath = get_attached_file( $image->ID );
 
 		if ( false === $fullsizepath || ! file_exists( $fullsizepath ) )
 			$this->die_json_error_msg( $image->ID, sprintf( __( 'The originally uploaded image file cannot be found at %s', 'flickr-shortcode-importer' ), '<code>' . esc_html( $fullsizepath ) . '</code>' ) );
 
-		@set_time_limit( 900 ); // 5 minutes per image should be PLENTY
+		@set_time_limit( 300 ); // 5 minutes per post should be PLENTY
 
 		$metadata = wp_generate_attachment_metadata( $image->ID, $fullsizepath );
 
@@ -335,13 +323,13 @@ class Flickr_Shortcode_Importer {
 		// If this fails, then it just means that nothing was changed (old value == new value)
 		wp_update_attachment_metadata( $image->ID, $metadata );
 
-		die( json_encode( array( 'success' => sprintf( __( '&quot;%1$s&quot; (ID %2$s) was successfully resized in %3$s seconds.', 'flickr-shortcode-importer' ), esc_html( get_the_title( $image->ID ) ), $image->ID, timer_stop() ) ) ) );
+		die( json_encode( array( 'success' => sprintf( __( '&quot;%1$s&quot; (ID %2$s) was successfully imported in %3$s seconds.', 'flickr-shortcode-importer' ), esc_html( get_the_title( $image->ID ) ), $image->ID, timer_stop() ) ) ) );
 	}
 
 
 	// Helper to make a JSON error message
 	function die_json_error_msg( $id, $message ) {
-		die( json_encode( array( 'error' => sprintf( __( '&quot;%1$s&quot; (ID %2$s) failed to resize. The error message was: %3$s', 'flickr-shortcode-importer' ), esc_html( get_the_title( $id ) ), $id, $message ) ) ) );
+		die( json_encode( array( 'error' => sprintf( __( '&quot;%1$s&quot; (ID %2$s) failed to be imported. The error message was: %3$s', 'flickr-shortcode-importer' ), esc_html( get_the_title( $id ) ), $id, $message ) ) ) );
 	}
 
 
@@ -350,6 +338,7 @@ class Flickr_Shortcode_Importer {
 		return str_replace( '"', '\"', $string );
 	}
 }
+
 
 // Start up this plugin
 function Flickr_Shortcode_Importer() {

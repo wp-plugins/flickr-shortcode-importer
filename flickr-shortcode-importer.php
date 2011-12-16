7ef2e89c6d3d2657317dcb223a9c6c5c65685d11
comprock <comprock@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-16 21:29:28 +0000

    Prep 1.4.9
    git-svn-id: https://plugins.svn.wordpress.org/flickr-shortcode-importer/trunk@476576 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- flickr-shortcode-importer.php
+++ flickr-shortcode-importer.php
@@ -3,7 +3,7 @@
 Plugin Name: Flickr Shortcode Importer
 Plugin URI: http://wordpress.org/extend/plugins/flickr-shortcode-importer/
 Description: Imports [flickr] & [flickrset] shortcode and Flickr-sourced A/IMG tagged media into the Media Library.
-Version: 1.4.8
+Version: 1.4.9
 Author: Michael Cannon
 Author URI: http://peimic.com/contact-peimic/
 License: GPL2
